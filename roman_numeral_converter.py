bf0b911e09a37834e9097d7f6a3ad09123317383
DESKTOP-6K2C8S3\RiverSafe <asad.khan@riversafe.co.uk>
2022-09-06 11:43:34 +0100

    roman numeral converter

--- roman_numeral_converter.py
+++ roman_numeral_converter.py
@@ -1,19 +1,23 @@
-
+from tester import printRoman
 
 
 def to_roman_numeral():
-    print("Arabic to Roman converter")
+    if __name__ == "__main__":
+        user_num = int(input("Enter a number: "))
+        print("Roman value is:", end = " ")
+        printRoman(user_num)
+
 
 
 
 def to_arabic_number():
     roman_input = input ("Enter roman number to convert: ")
 
-    if  "I" in roman_input :
+    if "I" in roman_input :
         roman_input = (int(1))
     elif "V" in roman_input :
         roman_input = (int(5)) 
-    elif "X" in rom :
+    elif "X" in roman_input :
         roman_input =(int(10))
     else:
      print("Invalid input")
@@ -24,4 +28,13 @@ def to_arabic_number():
 
 
 
-to_arabic_number()
+
+
+converter_option = input("Select which converter to use: ")
+
+if converter_option == ("1") :
+    to_roman_numeral()
+elif converter_option == ("2") :
+    to_arabic_number()
+else:
+    print("invalid input")
