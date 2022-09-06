ed057775addd0b4cb01343b60d460a82d36da679
DESKTOP-6K2C8S3\RiverSafe <asad.khan@riversafe.co.uk>
2022-09-06 09:56:28 +0100

    first converter commit

---
+++ roman_numeral_converter.py
@@ -0,0 +1,23 @@
+
+
+
+def to_roman_numeral():
+    print("Arabic to Roman converter")
+
+
+
+def to_arabic_number():
+    roman_input = input ("Enter roman number to convert: ")
+
+    if  roman_input == "I" :
+        print ("1") 
+    else:
+     print("Invalid input")
+
+    print("Roman to Arabic converter")
+
+
+
+
+
+to_arabic_number()
